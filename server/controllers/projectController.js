3f16587745ff58e7426730bf7aca688660fae132
AP <ap@Allisons-MBP.cable.rcn.com>
2020-06-24 12:21:39 -0400

    addProject working

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -24,18 +24,22 @@ projectController.getProjectDetails = (req, res, next) => {
 
 projectController.verifyProject = (req, res, next) => {
   const urlParams = req.body.url.split('/');
+  console.log(urlParams)
   res.locals.username = req.body.username;
+  res.locals.userid = req.body.id;
   res.locals.org = urlParams[urlParams.length - 2];
   res.locals.repo = urlParams[urlParams.length - 1];
   res.locals.description = req.body.description;
 
-  if (username !== org) {
-    fetch(`https://api.github.com/orgs/${res.locals.org}/members/`)
-      .then(res => {
-        if (res.status === 404) return res.send({ error: "You are not a member of this repo, get lost" });
+  if (res.locals.username !== res.locals.org) {
+    console.log('if')
+    fetch(`https://api.github.com/orgs/${res.locals.org}/members/${res.locals.username}`)
+      .then(data => {
+        if (data.status === 404) return res.send({ error: "You are not a member of this repo, get lost" });
         else return next();
       })
       .catch(err => {
+        console.log(err)
         return next({
           log: 'Error verifying repo',
           message: { err: 'projectController.verifyProject: error verifying repo'}
@@ -47,19 +51,22 @@ projectController.verifyProject = (req, res, next) => {
   }
 
   projectController.addProject = (req, res, next) => {
-    fetch(`https://api.github.com/${res.locals.org}/${res.locals.repo}`)
-      .then(res => res.json())
+    console.log(`https://api.github.com/repos/${res.locals.org}/${res.locals.repo}`)
+    fetch(`https://api.github.com/repos/${res.locals.org}/${res.locals.repo}`)
+      .then(data => data.json())
       .then(data => {
         res.locals.id = data.id;
-        res.locals.maintainer = data.owner.id;
-        const params = [res.locals.id, res.locals.maintainer, res.locals.repo, res.locals.description];
+        const params = [res.locals.id, res.locals.userid, res.locals.repo, res.locals.description];
         const query = `INSERT INTO projects (Id, Maintainer_id, Repo_name, Description) VALUES ($1, $2, $3, $4);`;
         db.query(query, params)
           .then(data => next())
-          .catch(err => next({
+          .catch(err => {
+            console.log(err)
+            return next({
             log: 'Error adding repo',
             message: { err: 'projectController.addProject: error adding repo' }
-          }))
+          })
+        })
         })
         .catch(err => next({
           log: 'Error fetching repo',
