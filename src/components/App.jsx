5d90fc989ec0bc9fe63f6bd3ce143a2b83798bdb
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-06-24 11:26:00 -0400

    added Reset.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Menu from "./Menu";
+import Outro from "./Outro";
+import "../styles/Reset.css";
 import "../styles/App.css";
 
 function App() {
   return (
     <>
       <Menu login={false} />
-      <h1 class="app_name">Open Sorcerer</h1>
-      <img src="fairy.png" />
+      <Outro />
     </>
   );
 }
---
+++ src/components/Outro.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Outro.css";
+
+function Outro() {
+  return <></>;
+}
+
+export default Outro;
