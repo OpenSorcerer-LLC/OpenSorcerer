740d4eaaa58962d44e339a7a6d8fcea62e11ab48
Derek Lauziere <dereklauziere@Dereks-Air.home>
2020-06-24 12:45:32 -0400

    oauth

--- server/server.js
+++ server/server.js
@@ -1,35 +1,45 @@
-express = require("express");
+express = require('express');
 app = express();
-const path = require("path");
-const cookieParser = require("cookie-parser");
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3000;
+const authController = require('./controllers/authController');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const projectRouter = require("./routes/projectRouter");
+const projectRouter = require('./routes/projectRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "../src/index.html"));
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
 });
 
-app.use("/verify", (req, res) => res.redirect("/"));
+app.get(
+  '/callback',
+  authController.getAccessToken,
+  authController.getUserInfo,
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
-app.use("/api", projectRouter);
+app.use('/verify', (req, res) => res.redirect('/'));
+
+app.use('/api', projectRouter);
 
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred(unhandled)' },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => console.log("server listening on port", PORT));
+app.listen(PORT, () => console.log('server listening on port', PORT));
